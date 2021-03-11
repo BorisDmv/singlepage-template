7a59982d0c384c58c77801fee213039b7c27e883
BORIS-DESKTOP\boris <borisdimitrov1998@gmail.com>
2021-03-11 16:17:43 +0200

    Mobile nav bar fixed and section 2 done

--- src/components/Section2.vue
+++ src/components/Section2.vue
@@ -23,40 +23,17 @@
               >
                 <v-card
                   :color="item.color"
-                  dark
+                  outlined
                 >
                   <div class="d-flex flex-no-wrap justify-space-between">
                     <div>
                       <v-card-title
-                        class="headline"
+                        class="headline primary--text"
                         v-text="item.title"
                       ></v-card-title>
 
-                      <v-card-subtitle v-text="item.artist"></v-card-subtitle>
+                      <v-card-subtitle class="text-left grey--text" v-text="item.artist"></v-card-subtitle>
 
-                      <v-card-actions>
-                        <v-btn
-                          v-if="item.artist === 'Ellie Goulding'"
-                          class="ml-2 mt-3"
-                          fab
-                          icon
-                          height="40px"
-                          right
-                          width="40px"
-                        >
-                          <v-icon>mdi-play</v-icon>
-                        </v-btn>
-
-                        <v-btn
-                          v-else
-                          class="ml-2 mt-5"
-                          outlined
-                          rounded
-                          small
-                        >
-                          START RADIO
-                        </v-btn>
-                      </v-card-actions>
                     </div>
 
                     <v-avatar
@@ -91,40 +68,76 @@ export default {
   data: () => ({
     items: [
         {
-          color: '#1F7087',
+          color: '#23273B',
           src: 'https://cdn.vuetifyjs.com/images/cards/foster.jpg',
           title: 'Supermodel',
           artist: 'Foster the People',
         },
         {
-          color: '#952175',
-          src: 'https://cdn.vuetifyjs.com/images/cards/halcyon.png',
-          title: 'Halcyon Days',
-          artist: 'Ellie Goulding',
+          color: '#23273B',
+          src: 'https://cdn.vuetifyjs.com/images/cards/foster.jpg',
+          title: 'Supermodel',
+          artist: 'Foster the People',
         },
         {
-          color: '#952175',
-          src: 'https://cdn.vuetifyjs.com/images/cards/halcyon.png',
-          title: 'Halcyon Days',
-          artist: 'Ellie Goulding',
+          color: '#23273B',
+          src: 'https://cdn.vuetifyjs.com/images/cards/foster.jpg',
+          title: 'Supermodel',
+          artist: 'Foster the People',
         },
         {
-          color: '#952175',
-          src: 'https://cdn.vuetifyjs.com/images/cards/halcyon.png',
-          title: 'Halcyon Days',
-          artist: 'Ellie Goulding',
+          color: '#23273B',
+          src: 'https://cdn.vuetifyjs.com/images/cards/foster.jpg',
+          title: 'Supermodel',
+          artist: 'Foster the People',
         },
         {
-          color: '#952175',
-          src: 'https://cdn.vuetifyjs.com/images/cards/halcyon.png',
-          title: 'Halcyon Days',
-          artist: 'Ellie Goulding',
+          color: '#23273B',
+          src: 'https://cdn.vuetifyjs.com/images/cards/foster.jpg',
+          title: 'Supermodel',
+          artist: 'Foster the People',
         },
         {
-          color: '#952175',
-          src: 'https://cdn.vuetifyjs.com/images/cards/halcyon.png',
-          title: 'Halcyon Days',
-          artist: 'Ellie Goulding',
+          color: '#23273B',
+          src: 'https://cdn.vuetifyjs.com/images/cards/foster.jpg',
+          title: 'Supermodel',
+          artist: 'Foster the People',
+        },
+        {
+          color: '#23273B',
+          src: 'https://cdn.vuetifyjs.com/images/cards/foster.jpg',
+          title: 'Supermodel',
+          artist: 'Foster the People',
+        },
+        {
+          color: '#23273B',
+          src: 'https://cdn.vuetifyjs.com/images/cards/foster.jpg',
+          title: 'Supermodel',
+          artist: 'Foster the People',
+        },
+        {
+          color: '#23273B',
+          src: 'https://cdn.vuetifyjs.com/images/cards/foster.jpg',
+          title: 'Supermodel',
+          artist: 'Foster the People',
+        },
+        {
+          color: '#23273B',
+          src: 'https://cdn.vuetifyjs.com/images/cards/foster.jpg',
+          title: 'Supermodel',
+          artist: 'Foster the People',
+        },
+        {
+          color: '#23273B',
+          src: 'https://cdn.vuetifyjs.com/images/cards/foster.jpg',
+          title: 'Supermodel',
+          artist: 'Foster the People',
+        },
+        {
+          color: '#23273B',
+          src: 'https://cdn.vuetifyjs.com/images/cards/foster.jpg',
+          title: 'Supermodel',
+          artist: 'Foster the People',
         },
       ]
   })
